b79e7edffde8a9190b224d5bd4286820fcf81d12
furka <matus.furka@stuba.sk>
2020-07-24 01:40:50 +0200

    update 1

---
+++ examples/problem_fed_batch/source/main_opt.m
@@ -0,0 +1,60 @@
+clear
+clc
+
+currpath = 'D:\VKzTAR\miro\dynopt_code-master';
+addpath([currpath,filesep,'dynoptim']);
+addpath([currpath,filesep,'adigator']);
+addpath([currpath,filesep,'adigator',filesep,'lib']);
+addpath([currpath,filesep,'adigator',filesep,'lib',filesep,'cadaUtils']);
+addpath([currpath,filesep,'adigator',filesep,'util']);
+addpath([currpath,filesep,'fminsdp']);
+addpath([currpath,filesep,'fminsdp',filesep,'interfaces']);
+addpath([currpath,filesep,'fminsdp',filesep,'utilities']);
+%% 
+options = sdpoptionset('LargeScale','on','Display','iter','TolFun',1e-7,...
+                       'TolCon',1e-7,'TolX',1e-7,...
+                       'MaxFunEvals',1e5,'MaxIter',150,'Algorithm','sqp',...
+                       'NLPsolver','fmincon');
+optimparam.optvar = 3;
+optimparam.objtype = [];
+optimparam.ncolx = 5;
+optimparam.ncolu = 2;
+optimparam.li = ones(10,1)*(1/1.5);
+optimparam.tf = [];
+optimparam.ui = ones(1,10)*6;
+optimparam.par = [];
+optimparam.bdu = [0 12];
+optimparam.bdx = [];
+optimparam.bdp = [];
+optimparam.objfun  = @objfun;
+optimparam.confun  = @confun;
+optimparam.process = @process;
+optimparam.options = options;
+[optimout, optimparam] = dynopt(optimparam);
+[tplot,uplot,xplot]    = profiles(optimout,optimparam,50);
+% [tp,cp,ceqp] = constraints(optimout,optimparam,50);
+
+%% plot
+figure
+hold on
+lines = {'k','b','r','g'};
+for i = 1:4
+    plot(tplot,xplot(:,i),lines{i})
+end
+legend('x_1','x_2','x_3','x_4')
+xlabel('time')
+ylabel('x_1, x_2, x_3, x_4')
+ylim([0 200])
+box on
+set(gca, 'FontSize', 12)
+% print -depsc 'batch_states.eps'
+hold off
+
+figure
+plot(tplot,uplot,'k')
+ylim([0 12])
+xlabel('time')
+ylabel('u')
+set(gca, 'FontSize', 12)
+box on
+% print -depsc 'batch_control.eps'
