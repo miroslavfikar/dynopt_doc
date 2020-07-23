811cbf7c9c87cfcf52dae4b37468cd61b75861a6
Michaela32 <michaela.horvathova@stuba.sk>
2020-07-23 09:03:02 +0200

    corrections

--- examples/problem10/process.m
+++ examples/problem10/process.m
@@ -4,7 +4,7 @@ function sys = process(t,x,flag,u,p)
    ki0 = [1e3; 1e7; 10; 1e-3];
    Ei  = [3000; 6000; 3000; 0];
    sys = [-(ki0(1)*exp(-Ei(1)/u))*x(1)-(ki0(2)*exp(-Ei(2)/u))*x(1);
-          (ki0(1)*exp(-Ei(1)/u))*x(1)-((ki0(3)*exp(-Ei(3)/u))+...
+          (ki0(1)*exp(-Ei(1)/u))*x(1)-(ki0(2)*exp(-Ei(2)/u)+(ki0(3)*exp(-Ei(3)/u))+...
           (ki0(4)*exp(-Ei(4)/u)))*x(2);
            ki0(3)*exp(-Ei(3)/u)*x(2)];
   elseif flag == 5  % initial conditions
