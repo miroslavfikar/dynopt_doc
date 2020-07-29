2e41b86833a10ca6b00c25c096534d7440c046f5
furka <matus.furka@stuba.sk>
2020-07-29 02:03:45 +0200

    update 2

--- examples/problem_fed_batch/source/main_opt.m
+++ examples/problem_fed_batch/source/main_opt.m
@@ -13,15 +13,15 @@ addpath([currpath,filesep,'fminsdp',filesep,'utilities']);
 %% 
 options = sdpoptionset('LargeScale','on','Display','iter','TolFun',1e-7,...
                        'TolCon',1e-7,'TolX',1e-7,...
-                       'MaxFunEvals',1e5,'MaxIter',150,'Algorithm','sqp',...
+                       'MaxFunEvals',1e5,'MaxIter',300,'Algorithm','sqp',...
                        'NLPsolver','fmincon');
 optimparam.optvar = 3;
 optimparam.objtype = [];
-optimparam.ncolx = 5;
+optimparam.ncolx = 7;
 optimparam.ncolu = 2;
-optimparam.li = ones(10,1)*(1/1.5);
+optimparam.li = ones(14,1)*(1/3);
 optimparam.tf = [];
-optimparam.ui = ones(1,10)*6;
+optimparam.ui = ones(1,14)*6;
 optimparam.par = [];
 optimparam.bdu = [0 12];
 optimparam.bdx = [];
@@ -45,16 +45,19 @@ legend('x_1','x_2','x_3','x_4')
 xlabel('time')
 ylabel('x_1, x_2, x_3, x_4')
 ylim([0 200])
+% xlim([0 60])
 box on
 set(gca, 'FontSize', 12)
-% print -depsc 'batch_states.eps'
+% print -depsc 'batch_states_dynopt.eps'
 hold off
 
 figure
 plot(tplot,uplot,'k')
 ylim([0 12])
+% xlim([0 60])
 xlabel('time')
 ylabel('u')
 set(gca, 'FontSize', 12)
 box on
-% print -depsc 'batch_control.eps'
+% print -depsc 'batch_control_dynopt.eps'
+J_opt = -optimout.fval
